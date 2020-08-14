64253836b37be13e639c1b48e71f157a2e34f589
zennynyc <61849993+zennynyc@users.noreply.github.com>
2020-08-13 23:01:41 -0400

    Delete twitter_credentials-checkpoint.py

--- .ipynb_checkpoints/twitter_credentials-checkpoint.py
+++
@@ -1,6 +0,0 @@
-# YouTube Video: https://www.youtube.com/watch?v=rhBZqEWsZU4
-# Variables that contains the user credentials to access Twitter API 
-ACCESS_TOKEN = "100997809-ph1Mxg4ylJ65Gt8a7olsVN4YJpA69cO6aH7UFStF"
-ACCESS_TOKEN_SECRET = "wIHNj3pRbKLv0L14XDikAqUuQ4WfO9yaELu2EoZ6WEua5"
-CONSUMER_KEY = "f2wYQJ84nt07qEWvDPK4BqMoS"
-CONSUMER_SECRET = "HpB4xueT1ZkioB3SJzwcCryBBmjcN39guzTt3XE8oMTj9EYDjq"
