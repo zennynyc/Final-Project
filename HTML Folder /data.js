6cf1bb2208d3f29e83e974ec069ddf22303e8b66
veronica-valencia <61913275+veronica-valencia@users.noreply.github.com>
2020-08-17 22:04:27 -0400

    Sentiment
    Sentiment Graph

---
+++ HTML Folder /data.js
@@ -0,0 +1,25 @@
+var data = {
+    Positive: {
+        School_Reopening: 2008,
+        Election: 2501,
+        Economy: 1584,
+        Mask_Safety: 2018,
+        General: 2590
+    },
+
+    Negative: {
+        School_Reopening: 3307,
+        Election: 3204,
+        Economy: 4036,
+        Mask_Safety: 4791,
+        General: 3436
+    },
+
+    Neutral: {
+        School_Reopening: 3242,
+        Election: 2240,
+        Economy: 1564,
+        Mask_Safety: 1841,
+        General: 1638
+    }
+};
